509831c9f4b438b8d98e03ab773a585c9695be1e
Brett Robinson <octo.banana93@gmail.com>
2020-01-05 19:11:09 -0700

    add variadic argument support to '+' function

--- src/ob/lispp.cc
+++ src/ob/lispp.cc
@@ -1133,28 +1133,37 @@ void env_init(std::shared_ptr<Env> ev, int argc, char** argv) {
     return a;
   }}, ev, builtin};
 
-  (*ev)["+"] = Val{Fun{str_lst("(a b)"), [&](auto e) -> Xpr {
+  (*ev)["+"] = Val{Fun{str_lst("(a b @)"), [&](auto e) -> Xpr {
+    auto const calc = [&](auto a, auto b) {
+      if (auto const lhs = xpr_num(&a)) {
+        if (auto const rhs = xpr_num(&b)) {
+          auto n = math(lhs, rhs, [](auto const a, auto const b) {return a + b;});
+          return num_normalize(n);
+        }
+      }
+      else if (auto const lhs = xpr_str(&a)) {
+        if (auto const rhs = xpr_str(&b)) {lhs->append(rhs->str()); return a;}
+        if (auto const rhs = xpr_num(&b)) {lhs->append(print(b)); return a;}
+        if (auto const rhs = xpr_sym(&b)) {lhs->append(print(b)); return a;}
+        if (auto const rhs = xpr_lst(&b)) {lhs->append(print(b)); return a;}
+      }
+      else if (auto const lhs = xpr_lst(&a)) {
+        if (auto const rhs = xpr_lst(&b)) {lhs->splice(lhs->end(), *rhs); return a;}
+        if (auto const rhs = xpr_num(&b)) {lhs->emplace_back(b); return a;}
+        if (auto const rhs = xpr_sym(&b)) {lhs->emplace_back(b); return a;}
+        if (auto const rhs = xpr_str(&b)) {lhs->emplace_back(b); return a;}
+      }
+      throw std::runtime_error("invalid types '" + typ_str.at(type(a)) + "' and '" + typ_str.at(type(b)) + "'");
+    };
     auto a = eval(sym_xpr("a"), e);
     auto b = eval(sym_xpr("b"), e);
-    if (auto const lhs = xpr_num(&a)) {
-      if (auto const rhs = xpr_num(&b)) {
-        auto n = math(lhs, rhs, [](auto const a, auto const b) {return a + b;});
-        return num_normalize(n);
-      }
+    a = calc(a, b);
+    auto x = eval(sym_xpr("@"), e);
+    auto& l = std::get<Lst>(x);
+    for (auto it = l.begin(); it != l.end(); ++it) {
+      a = calc(a, eval(*it, e->current));
     }
-    else if (auto const lhs = xpr_str(&a)) {
-      if (auto const rhs = xpr_str(&b)) {lhs->append(rhs->str()); return a;}
-      if (auto const rhs = xpr_num(&b)) {lhs->append(print(b)); return a;}
-      if (auto const rhs = xpr_sym(&b)) {lhs->append(print(b)); return a;}
-      if (auto const rhs = xpr_lst(&b)) {lhs->append(print(b)); return a;}
-    }
-    else if (auto const lhs = xpr_lst(&a)) {
-      if (auto const rhs = xpr_lst(&b)) {lhs->splice(lhs->end(), *rhs); return a;}
-      if (auto const rhs = xpr_num(&b)) {lhs->emplace_back(b); return a;}
-      if (auto const rhs = xpr_sym(&b)) {lhs->emplace_back(b); return a;}
-      if (auto const rhs = xpr_str(&b)) {lhs->emplace_back(b); return a;}
-    }
-    throw std::runtime_error("invalid types '" + typ_str.at(type(a)) + "' and '" + typ_str.at(type(b)) + "'");
+    return a;
   }}, ev, builtin};
 
   (*ev)["-"] = Val{Fun{str_lst("(a b)"), [&](auto e) -> Xpr {
